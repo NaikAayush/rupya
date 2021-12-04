3ea676520f7616c0762ecc89adc8316ba8e416d1
NaikAayush <ayush@pesu.pes.edu>
2021-12-04 17:27:35 +0530

    feat: navbar

--- faucet-webapp/src/app/app.module.ts
+++ faucet-webapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WalletConnectComponent } from './components/wallet-connect/wallet-connect.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WalletConnectComponent
+    WalletConnectComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
