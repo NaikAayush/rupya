c009bfd921e46dc45a356b262cb809c8509b4224
NaikAayush <ayush@pesu.pes.edu>
2021-12-04 16:41:03 +0530

    feat: wallet connect modal

--- faucet-webapp/src/app/app.module.ts
+++ faucet-webapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WalletConnectComponent } from './components/wallet-connect/wallet-connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WalletConnectComponent
   ],
   imports: [
     BrowserModule,
