66e4719728749635798b65b2317295f966da4933
NaikAayush <ayush@pesu.pes.edu>
2021-12-04 15:56:12 +0530

    feat: tailwind setup

---
+++ faucet-webapp/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: "",
+  purge: {
+    enabled: false,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
