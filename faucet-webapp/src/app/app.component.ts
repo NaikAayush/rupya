a5b19e0bfe78dd0efaf75f1570556db01cb6f85c
NaikAayush <ayush@pesu.pes.edu>
2021-12-04 15:49:09 +0530

    feat: setup web3

--- faucet-webapp/src/app/app.component.ts
+++ faucet-webapp/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3';
+declare let window: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'faucet-webapp';
+
+  constructor() {
+    this.loadWeb3();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      await window.ethereum.send('eth_requestAccounts');
+      window.web3 = new Web3(window.ethereum);
+      return true;
+    }
+    return false;
+  }
 }
