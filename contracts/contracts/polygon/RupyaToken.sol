a9f950a5691003bcd02ea2c675ba31b3aeb39548
NaikAayush <ayush@pesu.pes.edu>
2021-12-07 14:34:17 +0530

    feat: rupya token contract and deploy script

--- contracts/contracts/polygon/RupyaToken.sol
+++ contracts/contracts/polygon/RupyaToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
+contract RupyaToken is ERC20 {
     constructor() ERC20("Rupya", "RUP") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
