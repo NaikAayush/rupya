b1d3a59eee1dddbba53ea3cf647ce2ff1bde704f
NaikAayush <ayush@pesu.pes.edu>
2021-12-08 10:42:50 +0530

    feat: rupya token and usdc token with faucet function

--- contracts/contracts/polygon/tokens/RupyaToken.sol
+++ contracts/contracts/polygon/tokens/RupyaToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RupyaToken is ERC20 {
     constructor() ERC20("Rupya", "RUP") {
