8197537ceae2ee27c9356e07c970345da77c732b
NaikAayush <ayush@pesu.pes.edu>
2021-12-08 12:31:34 +0530

    fix: ui fixes

--- faucet-webapp/src/app/components/faucet/faucet.component.ts
+++ faucet-webapp/src/app/components/faucet/faucet.component.ts
@@ -7,7 +7,7 @@ import { FaucetService } from 'src/app/services/faucet/faucet.service';
   styleUrls: ['./faucet.component.css'],
 })
 export class FaucetComponent implements OnInit {
-  address: string = '0x6D209f66bbE4E075BC4bA51B184E5669cF065268';
+  address: string = '';
   token: string = 'rupya';
   constructor(private faucet: FaucetService) {}
 
