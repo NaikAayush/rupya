60a13d7037d1db85bcfd32f38dbce80091d1473f
NaikAayush <ayush@pesu.pes.edu>
2021-12-08 12:24:37 +0530

    fix: add usdc in dropdown

--- faucet-webapp/src/app/components/faucet/faucet.component.ts
+++ faucet-webapp/src/app/components/faucet/faucet.component.ts
@@ -7,12 +7,14 @@ import { FaucetService } from 'src/app/services/faucet/faucet.service';
   styleUrls: ['./faucet.component.css'],
 })
 export class FaucetComponent implements OnInit {
-  address: string = '0x66ba284A8ad145b788543643A96B5b40058d4637';
+  address: string = '0x6D209f66bbE4E075BC4bA51B184E5669cF065268';
+  token: string = 'rupya';
   constructor(private faucet: FaucetService) {}
 
   ngOnInit(): void {}
 
   getToken() {
-    this.faucet.getToken(this.address);
+    this.faucet.getToken(this.address, this.token);
+    // console.log(this.token);
   }
 }
