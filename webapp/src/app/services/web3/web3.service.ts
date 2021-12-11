e5579e47a447e73092b7a2a2eac68e07ccfb5eda
NaikAayush <ayush@pesu.pes.edu>
2021-12-11 19:36:06 +0530

    feat: decode data function

--- webapp/src/app/services/web3/web3.service.ts
+++ webapp/src/app/services/web3/web3.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import Web3 from 'web3';
 import { environment } from 'src/environments/environment';
 declare let window: any;
+import Web3Abi, { AbiCoder } from 'web3-eth-abi';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Web3Service {
   web3: any;
+
   address: any;
   constructor() {
-    // this.web3 = new Web3(environment.providerURL);
+    this.web3 = new Web3();
   }
 
   async isLoggedIn() {
@@ -46,6 +48,26 @@ export class Web3Service {
         'Non-Ethereum browser detected. You should consider trying MetaMask!'
       );
     }
+  }
+
+  async decodeData(typesArray: Array<Object>, raw_log_data: string) {
+    const abiCoder = Web3Abi as unknown as AbiCoder;
+    // const res = await this.covalent.getEvent();
+    // console.log(res.raw_log_topics[0]);
+    // console.log(res.raw_log_data);
+
+    // Demo types array
+    // const typesArray = [
+    //   { type: 'address', name: '_From' },
+    //   { type: 'string', name: 'username' },
+    //   { type: 'string', name: 'ipfsHash' },
+    // ];
+
+    const decodedParameters = abiCoder.decodeParameters(
+      typesArray,
+      raw_log_data
+    );
+    console.log(decodedParameters);
   }
 
   // async logout() {
